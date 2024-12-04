1cf1962fbd79f79ddb199c3d6012ba021bb17a24
Sup#2.0 <sup2point0@gmail.com>
2024-12-04 20:39:29 +0000

    upgrade to Svelte 5

--- svelte.config.js
+++ svelte.config.js
@@ -29,7 +29,7 @@ const config = {
   preprocess: [
     sveltePreprocess({
       scss: {
-        prependData: ``,
+        prependData: `@use './src/styles/mixins/colours' as *;`,
       }
     }),
   ],
