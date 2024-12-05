004120b715ee81cca084d6b39b34cd1e7c0b10dc
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-12-05 10:01:20 +0000

    add `#parts/nav.svelte`

---
+++ site/src/parts/core/nav.svelte
@@ -0,0 +1,40 @@
+<!-- @component Nav -->
+
+<script lang="ts">
+
+import NavLink from "#parts/core/nav.link.svelte";
+
+</script>
+
+
+<nav>
+  <NavLink pict="integrity.png" />
+
+  <NavLink text="Integrals" intern="integrals" />
+  <NavLink text="Graph Drawing" intern="graphs" />
+  <NavLink text="Completing the Square" intern="complete-the-square" />
+  <NavLink text="Addvent" intern="addvent" />
+
+  <NavLink text="GitHub" link="https://github.com/Sup2point0/integrity" />
+</nav>
+
+
+<style lang="scss">
+
+nav {
+  width: 100%;
+  margin-bottom: 1rem;
+  padding: 0.5rem 2rem;
+  position: sticky;
+  top: 0;
+  z-index: 20;
+  display: flex;
+  flex-direction: row;
+  justify-content: stretch;
+  align-items: center;
+  gap: 0.5rem;
+  background: transparent;
+  backdrop-filter: blur(16px) brightness(0.96);
+}
+
+</style>
