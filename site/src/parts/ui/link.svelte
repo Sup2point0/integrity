c3f88ec485197462ce84c10af45c42db4b5cba1e
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 20:56:29 +0000

    add `#parts/link.svelte`

---
+++ site/src/parts/ui/link.svelte
@@ -0,0 +1,40 @@
+<!-- @component Link
+
+A generic block-level link. -->
+
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+
+interface Props {
+  text: string;
+  link?: string;
+  intern?: string;
+}
+
+let { text, link, intern }: Props = $props();
+
+</script>
+
+
+<a class="link"
+  href="{link || `${base}/${intern}`}"
+>
+  {text}
+</a>
+
+
+<style lang="scss">
+
+a.link {
+  padding: 0.5em;
+  color: $col-prot;
+  font-weight: 300;
+  text-decoration: none;
+  border-radius: 0.5em;
+
+  @include interact($col-hover, $col-click);
+}
+
+</style>
