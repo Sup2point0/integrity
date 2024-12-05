63f9a72f8733710f35cfd8bcb37c6521f5cfb520
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 18:56:24 +0000

    add `#parts/katex.svelte`

---
+++ site/src/parts/katex.svelte
@@ -0,0 +1,34 @@
+<!-- @component Katex
+
+LaTeX rendered by KaTeX. -->
+
+<script lang="ts">
+
+import katex from "katex";
+
+
+interface Props {
+  text: string;
+  inline: boolean;
+}
+
+let { text, inline = true }: Props = $props();
+const opts = {
+  displayMode: !inline,
+  throwOnError: false,
+}
+
+
+let out = $derived(katex.renderToString(text, opts))
+
+</script>
+
+
+{@html out}
+
+
+<style lang="scss">
+
+
+
+</style>
