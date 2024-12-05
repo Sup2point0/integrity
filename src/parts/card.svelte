85af3ae255fe3b00df82d2d40a42d2ad16eb6820
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 07:25:15 +0000

    add `#parts/card.svelte`

---
+++ src/parts/card.svelte
@@ -0,0 +1,48 @@
+<!-- @component Card
+
+-->
+
+<script lang="ts">
+
+import Tag from "#parts/tag.svelte";
+
+import { base } from "$app/paths";
+
+
+interface Props {
+  intern: string;
+  title: string;
+  date: string;
+  tags: string[];
+}
+
+let {
+  title,
+  date = "",
+  tags = [],
+  intern,
+}: Props = $props();
+
+</script>
+
+
+<a class="card"
+  href="{base}/{intern}"
+>
+  <div class="integral"></div>
+  <div class="info">
+    <h4> {title} </h4>
+    <p> {date} </p>
+
+    {#each tags as tag}
+      <Tag shard={tag} />
+    {/each}
+  </div>
+</a>
+
+
+<style lang="scss">
+
+
+
+</style>
