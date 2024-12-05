965b5f7e6c6e3eede69811b3131c10e6b5364135
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 07:25:24 +0000

    setup layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+
+import { fade } from "svelte/transition";
+import { onMount } from "svelte";
+
+
+let active = true;
+
+
+onMount(() => {
+  setTimeout(() => {
+    active = false;
+  }, 1000);
+})
+
+</script>
+
+
+<slot />
+
+{#if active}
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <aside class="overlay"
+    on:click={() => { active = false; }}
+    transition:fade={{ duration: 500 }}
+  >
+    <div class="content">
+      <h1> integrity </h1>
+      <p> by Sup#2.0 </p>
+    </div>
+  </aside>
+{/if}
+
+
+<style lang="scss">
+
+aside.overlay {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: $col-prot;
+}
+
+div.content {
+  text-align: center;
+}
+
+
+h1 {
+  color: white;
+}
+
+p {
+  color: white;
+}
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+import Site from "#scripts/site";
+
+import Card from "#parts/card.svelte";
+
+</script>
+
+
+{#each Site.integrals as question}
+  <Card
+    intern={question.shard}
+    title={question.title}
+    date={question.date}
+    tags={question.tags}
+  />
+{/each}
+
+
+<style lang="scss">
+
+
+
+</style>
