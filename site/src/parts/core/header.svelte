fa2e7419fe362cff6f2dcbd0e2d4bf9a3be7c85f
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 19:14:20 +0000

    add `#parts/header.svelte`

---
+++ site/src/parts/core/header.svelte
@@ -0,0 +1,32 @@
+<!-- @component Header -->
+
+<script lang="ts">
+
+interface Props {
+  title: string;
+}
+
+let { title }: Props = $props();
+
+</script>
+
+
+<header>
+  <h1> {title} </h1>
+</header>
+
+
+<style lang="scss">
+
+header {
+  width: 100%;
+  padding: 2rem 0 4rem;
+  text-align: center;
+}
+
+h1 {
+  @include font-serif;
+  font-size: 250%;
+}
+
+</style>
