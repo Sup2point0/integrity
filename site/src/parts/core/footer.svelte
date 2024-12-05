a6bb443fb681185b70e071f6615d6274af3ddfd0
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-12-05 09:55:26 +0000

    add `#parts/footer.svelte`

---
+++ site/src/parts/core/footer.svelte
@@ -0,0 +1,73 @@
+<!-- @component Footer -->
+
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+</script>
+
+
+<footer>
+  <div class="content">
+
+    <div class="left">
+      <img id="integrity-title"
+        alt="integrity’"
+        src="{base}/title.svg" />
+    </div>
+  
+    <div class="right">
+      <a class="credits" href="https://github.com/Sup2point0">
+        <div>
+          <p> made with love by </p>
+          <p id="sup2point0"> Sup#2.0 </p>
+        </div>
+  
+        <img id="purple-portal"
+          alt="Sup#2.0"
+          src="{base}/purple-portal.png" />
+      </div>
+    </div>
+
+  </div>
+</footer>
+
+
+<style lang="scss">
+
+footer {
+  width: 100%;
+  padding: 1rem 0 4rem;
+
+  .content {
+    width: 80%;
+    max-width: 80rem;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+  }
+}
+
+.left { text-align: left; }
+.right { text-align: right; }
+
+a.credits {
+  text-decoration; none;
+
+  @include interact;
+  @include focus;
+}
+
+
+img#integrity-title {
+  max-width: 20rem;
+}
+
+img#purple-portal {
+  max-width: 10rem;
+}
+
+</style>
