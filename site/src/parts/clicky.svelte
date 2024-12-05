a7705b29905d7321c483ab913b49635351596beb
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-12-05 14:32:35 +0000

    add `#parts/clicky.svelte`

---
+++ site/src/parts/clicky.svelte
@@ -0,0 +1,60 @@
+<!-- @component Clicky -->
+
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+
+interface Props {
+  text: string;
+  link?: string;
+  intern?: string;
+  button?: () => void;
+}
+
+let { text, link, intern, button }: Props = $props();
+
+</script>
+
+
+{#if link}
+  <a class="clicky"
+    href={link || `${base}/${intern}`}
+  >
+    <button>
+      {text}
+    </button>
+  </a>
+
+  {:else}
+  <button class="clicky"
+    onclick={button}
+  >
+    {text}
+  </button>
+
+{/if}
+
+
+<style lang="scss">
+
+.clicky {
+  padding: 0.4em 0.8em;
+  background: white;
+  border: 1px solid $col-line;
+  border-radius: 0.5em;
+  box-shadow: 0 1.5px 2px -0.5px $col-line;
+  transition: all 0.1s ease-out;
+
+  &:hover {
+    cursor: pointer;
+    background-color: oklch(98% 0 0);
+  }
+
+  &:active {
+    cursor: pointer;
+    background-color: oklch(95% 0 0);
+  }
+}
+
+</style>
