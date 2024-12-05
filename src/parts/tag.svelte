c06c13e0f473e544310abb83acbeaccb3215410b
Sup#2.0 <sup2point0@gmail.com>
2024-12-05 07:25:19 +0000

    add `#parts/tag.svelte`

---
+++ src/parts/tag.svelte
@@ -0,0 +1,30 @@
+<!-- @component Tag
+
+-->
+
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+
+interface Props {
+  shard: string;
+}
+
+let { shard }: Props = $props();
+
+</script>
+
+
+<a href="{base}/search?tag={shard}" class="tag">
+  {shard.toUpperCase()}
+</a>
+
+
+<style lang="scss">
+
+a {
+  text-decoration: none;
+}
+
+</style>
