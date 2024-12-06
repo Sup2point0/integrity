c9f02c8b573af0b3000166e49e539516ca4781c4
Sup#2.0 <sup2point0@gmail.com>
2024-12-06 18:25:36 +0000

    add article pages

---
+++ site/src/routes/meta/decoded/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+import Content from "../../../../../edu.md";
+
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<svelte:head>
+  <title> decoded · Integrity · Sup#2.0 </title>
+  <meta name="description"
+    content="Learnings, challenges and discoveries from developing integrity." />
+</svelte:head>
+
+
+<Header> decoded: integrity </Header>
+
+<Content />
+
+
+<style lang="scss">
+
+@use './src/styles/article.svelte.scss' as *;
+
+</style>
---
+++ site/src/routes/meta/synopsis/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+import Content from "../../../../../synopsis.md";
+
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<svelte:head>
+  <title> Synopsis · Integrity · Sup#2.0 </title>
+  <meta name="description"
+    content="Learnings, challenges and discoveries from developing Integrity." />
+</svelte:head>
+
+
+<Header> Synopsis </Header>
+
+<Content />
+
+
+<style lang="scss">
+
+@use './src/styles/article.svelte.scss' as *;
+
+</style>
