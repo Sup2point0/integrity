43147d9f63256d08faeedba53fb6572fef26dae2
Sup#2.0 <sup2point0@gmail.com>
2024-12-06 20:30:40 +0000

    implement question parsing engine

--- scripts/process.rb
+++ scripts/process.rb
@@ -10,20 +10,41 @@ def process(filepath:)
 
   ## FIXME
   section = nil
+  subsection = nil
   load = nil
 
   lines.each do |line|
-    if line.start_with?("## ")
-      if section and load
-        data[section] = extract_groups(load)
+    # if new heading, save current load
+    if line.start_with?("##")
+      if load and not load.empty?
+        if subsection
+          if data[section].nil? then data[section] = {} end
+          data[section][subsection] = extract_groups(load)
+        elsif section
+          data[section] = extract_groups(load)
+        end
       end
 
+    else
+      # if in a section, add to load
+      if section and not line.strip.empty?
+        load.push(line)
+      end
+    end
+
+    # start new section
+    if line.start_with?("## ")
       _, _, section = line.partition("## ")
+      subsection = nil
       load = []
     end
-    
-    if section
-      load.push(line)
+
+    # start new subsection
+    if line.start_with?("### ")
+      if section
+        _, _, subsection = line.partition("### ")
+        load = []
+      end
     end
   end
 
@@ -37,26 +58,36 @@ def extract_groups(lines)
   ctx = nil
 
   lines.each do |line|
-    if ctx == "math"
+    if ctx == "latex"
       if line.end_with?("```")
         out.push({
           "kind" => "latex",
           "content" => load,
         })
         ctx = nil
+        next
       else
         load.concat(line)
       end
     else
-      if line
+      if not line.strip.empty?
         load.concat(line)
       end
     end
 
     if line.start_with?("```math")
-      ctx = "math"
+      ctx = "latex"
       load = ""
+    elsif ctx.nil? and not line.strip.empty?
+      ctx = "text"
     end
+  end
+
+  if not ctx.nil?
+    out.push({
+      "kind" => "text",
+      "content" => load,
+    })
   end
 
   return out
