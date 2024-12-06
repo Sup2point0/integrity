0d603dd894be341211cbeff30f922a0e7ebc9644
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-12-06 10:34:00 +0000

    add pages

---
+++ site/src/routes/complete-the-square/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+
+import QuestionCard from "#parts/ui/card.question.svelte";
+
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Header> Completing the Square </Header>
+
+<div class="content">
+  <QuestionCard
+    title="Testing"
+    latex={"\\int_{a}^{b} f(x) \\, dx"}
+    date="2024 December"
+    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
+  />
+  <QuestionCard
+    title="Testing"
+    latex={"\\int_{a}^{b} f(x) \\, dx"}
+    date="2024 December"
+    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
+  />
+  <QuestionCard
+    title="Testing"
+    latex={"\\int_{a}^{b} f(x) \\, dx"}
+    date="2024 December"
+    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
+  />
+  <QuestionCard
+    title="Testing"
+    latex={"\\int_{a}^{b} f(x) \\, dx"}
+    date="2024 December"
+    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
+  />
+  <QuestionCard
+    title="Testing"
+    latex={"\\int_{a}^{b} f(x) \\, dx"}
+    date="2024 December"
+    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
+  />
+</div>
+
+
+<style lang="scss">
+
+.content {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+</style>
