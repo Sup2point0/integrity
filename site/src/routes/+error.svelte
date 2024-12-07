7198db8b2085c678228cf028707be5a54600765a
Sup#2.0 <sup2point0@gmail.com>
2024-12-07 12:49:20 +0000

    add `#routes/+error.svelte`

---
+++ site/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+import { page } from "$app/stores";
+
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Header> {$page.status ?? "Internal Error"} </Header>
+
+<p> {$page.error.message} </p>
