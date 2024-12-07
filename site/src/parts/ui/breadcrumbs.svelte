4e1a2a4cf8c8fc5ba986d86b0fec857d0657c599
Sup#2.0 <sup2point0@gmail.com>
2024-12-07 18:30:38 +0000

    add `#parts/breadcrumbs.svelte`

---
+++ site/src/parts/ui/breadcrumbs.svelte
@@ -0,0 +1,31 @@
+<!-- @component Breadcrumbs
+
+A bar of slash-separated `<Link>`s for navigation.
+-->
+
+<script lang="ts">
+
+let { children } = $props();
+
+</script>
+
+
+<nav class="breadcrumbs">
+  {@render children?.()}
+</nav>
+
+
+<style lang="scss">
+
+nav.breadcrumbs {
+  padding: 0.5em 0;
+
+  > :global(a.link:not(:first-child))::before {
+    content: "/";
+    margin: 0 0.5em;
+    @include font-ui;
+    color: $col-text-deut;
+  }
+}
+
+</style>
