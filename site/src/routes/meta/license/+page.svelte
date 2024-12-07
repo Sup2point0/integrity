41158ec1ba2fa91a16452ad728302b9637d6b252
Sup#2.0 <sup2point0@gmail.com>
2024-12-07 11:48:28 +0000

    add license page

---
+++ site/src/routes/meta/license/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+import Content from "./content.md";
+
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<svelte:head>
+  <title> License · Integrity </title>
+  <meta name="description"
+    content="Regarding usage of content from Integrity" />
+</svelte:head>
+
+
+<Header title="License"
+  desc="Usage of content from Integrity"
+/>
+
+<article>
+  <Content />
+</article>
---
+++ site/src/routes/meta/license/content.md
@@ -0,0 +1,3 @@
+*Integrity* is licensed under the [MIT License](https://github.com/Sup2point0/integrity/blob/main/LICENSE).
+
+Maths is for everyone, so you’re more than welcome to use and redistribute any of the questions in *Integrity*. That being said, if you’re using them in bulk, some credit and/or linking back to *Integrity* would be much appreciated!
