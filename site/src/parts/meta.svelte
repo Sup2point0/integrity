85e92e3e755739d4ec52958b28603b009c255ad3
Sup#2.0 <sup2point0@gmail.com>
2024-12-09 18:25:38 +0000

    add `#parts/meta.svelte`

---
+++ site/src/parts/meta.svelte
@@ -0,0 +1,21 @@
+<!-- @component Meta
+
+Abstraction for configuring page meta tags in `<head>`.
+-->
+
+<script lang="ts">
+
+interface Props {
+  title?: string;
+  desc?: string;
+}
+
+let { title, desc }: Props = $props();
+
+</script>
+
+
+<svelte:head>
+  <title> {title} · Integrity </title>
+  <meta name="description" content="{desc}" />
+</svelte:head>
