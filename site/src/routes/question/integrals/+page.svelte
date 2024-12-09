ec441063b32be49dd6dbf866496dc6687aabfc20
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-12-09 14:58:20 +0000

    add integral pages

---
+++ site/src/routes/question/integrals/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+
+import Site from "#scripts/site";
+import type { Question } from "#scripts/types";
+
+import Katex from "#parts/katex.svelte";
+import RenderBlock from "#parts/render-block.svelte";
+
+import Header from "#parts/core/header.svelte";
+import Breadcrumbs from "#parts/ui/breadcrumbs.svelte";
+import Section from "#parts/section.svelte";
+
+import { page } from "$app/stores";
+import { onMount } from "svelte";
+import { error } from "@sveltejs/kit";
+
+
+let loaded = false;
+let shard: string | null = null;
+let question: Question | null = null;
+
+
+onMount(() => {
+  let params = $page.url.searchParams;
+
+  shard = params.get("shard");
+  if (shard == null) {
+    error(404, { message: "Question not found!" });
+  }
+
+  question = Site.questions["integrals"].questions[shard!];
+  if (question == null) {
+    error(404, { message: "Question not found!" });
+  }
+
+  loaded = true;
+});
+
+</script>
+
+
+{#if loaded && question}
+  <Breadcrumbs levels={[
+    { text: "Integrals", intern: "questions/integrals" },
+    { text: shard ?? "?" },
+  ]} />
+
+  <section class="question">
+    <Katex text={question.question.content} />
+  </section>
+
+  {#if question.hints}
+    <Section title="Hints">
+      {#each Object.entries(question.hints) as [hint, source]}
+        <Section ctx="inner" title={hint}>
+          <RenderBlock {source} />
+        </Section>
+      {/each}
+    </Section>
+  {/if}
+
+  {#if question.answer}
+    <Section title="Answer">
+      <RenderBlock source={question.answer} />
+    </Section>
+  {/if}
+
+  {#if question.solution}
+    <Section title="Solution">
+      {#each Object.entries(question.solution) as [step, source]}
+        <Section ctx="inner" closed={false} title={step}>
+          <RenderBlock {source} />
+        </Section>
+      {/each}
+    </Section>
+  {/if}
+
+{:else}
+  <p> Hold tight, loading... </p>
+
+{/if}
+
+
+<style lang="scss">
+
+section {
+  text-align: center;
+
+  &.question {
+    margin: 4rem 0;
+    font-size: 150%;
+  }
+}
+
+</style>
--- site/src/routes/questions/integrals/+page.svelte
+++ site/src/routes/questions/integrals/+page.svelte
@@ -1,9 +1,28 @@
 <script lang="ts">
 
+import Site from "#scripts/site";
+import { search } from "#scripts/stores";
+import { filter_questions } from "#scripts/search";
+
 import QuestionCard from "#parts/ui/card.question.svelte";
 
 import Header from "#parts/core/header.svelte";
 import Search from "#parts/ui/search.svelte";
+
+import { onMount } from "svelte";
+
+
+const tags = Site.questions["integrals"].tags;
+
+let filtered = $derived(filter_questions(
+  Site.get_questions("integrals"),
+  search
+));
+
+
+onMount(() => {
+  search.tags = Object.fromEntries(tags.map(tag => [tag, false]));
+})
 
 </script>
 
@@ -15,75 +34,22 @@ import Search from "#parts/ui/search.svelte";
 
 <Header title="Integrals" />
 
-<Search tags={["trig", "horror", "sub", "long", "integral", "parts"]} />
+<Search {tags} />
 
 <div class="content">
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
-  <QuestionCard
-    title="Testing"
-    latex={"\\int_{a}^{b} f(x) \\, dx"}
-    date="2024 December"
-    tags={["trig", "horror", "sub", "long", "integral", "parts"]}
-  />
+  {#each filtered as q}
+    <QuestionCard
+      title={q.title}
+      intern="question/integrals?shard={q.shard}"
+      latex={q.question.content}
+      date={search.show.dates ? q.date : undefined}
+      tags={search.show.tags ? q.tags : undefined}
+    />
+  {/each}
+
+  {#if filtered.length === 0}
+    <p> Oops, no questions found! </p>
+  {/if}
 </div>
 
 
@@ -95,6 +61,10 @@ import Search from "#parts/ui/search.svelte";
   justify-content: center;
   flex-wrap: wrap;
   gap: 1rem;
+}
+
+p {
+  color: $col-text-deut;
 }
 
 </style>
