a9f10b9d9a7ccc465560a660ac681870ddb41aad
Sup#2.0 <sup2point0@gmail.com>
2024-12-11 18:54:17 +0000

    add `#parts/select.svelte`

---
+++ site/src/parts/ui/select.svelte
@@ -0,0 +1,47 @@
+<!-- @component Select
+
+A select dropdown input.
+-->
+
+<script lang="ts">
+
+interface Props {
+  options: string[] | {
+    [key: string]: string;
+  };
+  value: any;
+}
+
+let { options, value = $bindable() }: Props = $props();
+
+</script>
+
+
+<select bind:value>
+  {#each (
+    Array.isArray(options) ? options.map(opt => [opt, opt]) : Object.entries(options)
+  ) as [option, value]}
+    <option {value}> {option} </option>
+  {/each}
+</select>
+
+
+<style lang="scss">
+
+select {
+  appearance: none;
+  padding: 0.4em 0.8em;
+  @include font-ui;
+  background-color: white;
+  border: 1px solid $col-line;
+  border-radius: 0.5em;
+  box-shadow: 0 1.5px 2px -0.5px $col-line;
+
+  @include interact(
+    $hover: oklch(98% 0 0),
+    $click: oklch(95% 0 0),
+    $t: 0.12,
+  );
+}
+
+</style>
