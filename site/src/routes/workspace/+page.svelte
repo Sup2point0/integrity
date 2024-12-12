8f11d629567e1cd5320b007f6328c5507b2cdc3f
Sup#2.0 <sup2point0@gmail.com>
2024-12-12 11:57:40 +0000

    add `#routes/workspace`

---
+++ site/src/routes/workspace/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+import { onMount } from "svelte";
+
+
+let calc = null;
+
+onMount(() => {
+  if (typeof Desmos !== "undefined") {
+    calc = Desmos.GraphingCalculator(
+      document.getElementById("desmos-window")
+    );
+  }
+});
+
+</script>
+
+
+<div id="desmos-window"></div>
+
+
+<style lang="scss">
+
+#desmos-window {
+  width: 100%;
+  max-height: 80vh;
+}
+
+</style>
