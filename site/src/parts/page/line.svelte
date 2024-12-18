afa79fa5d8b22e4043e4f18dbf6dd2b88641d5c6
Sup#2.0 <sup2point0@gmail.com>
2024-12-18 21:29:42 +0000

    add `#parts/line.svelte`

---
+++ site/src/parts/page/line.svelte
@@ -0,0 +1,35 @@
+<!-- @component Line
+
+A lovely horizontal line that fills up a specified width of its container.
+-->
+
+<script lang="ts">
+
+interface Props {
+  width?: string;
+  margin?: string;
+}
+
+let {
+  width = "100%",
+  margin = "4rem",
+}: Props = $props();
+
+</script>
+
+
+<div class="line"
+  style:width
+  style:margin-top={margin}
+  style:margin-bottom={margin}
+>
+</div>
+
+
+<style lang="scss">
+
+.line {
+  border-bottom: 1px solid $col-line;
+}
+
+</style>
