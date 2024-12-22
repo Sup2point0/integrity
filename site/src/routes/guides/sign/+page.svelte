2241faed78067354369e4f4e75f602ba2e848aee
Sup#2.0 <sup2point0@gmail.com>
2024-12-22 09:24:52 +0000

    add `guides/sign`

---
+++ site/src/routes/guides/sign/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+
+import Content from "./content.svx";
+import { metadata as page } from "./content.svx";
+
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "S-I+G-N Errors" }
+]} />
+
+<Header title={page.title} capt={page.capt} />
+
+<article>
+  <Content />
+</article>
