3d7d55267a7556ad1e3660edb3ab0558d32cfc08
Sup#2.0 <sup2point0@gmail.com>
2024-12-29 09:05:36 +0000

    add `#parts/copy-clicky.svelte`

---
+++ site/src/parts/page/copy-clicky.svelte
@@ -0,0 +1,58 @@
+<!-- @component CopyClicky
+
+A button to copy something to clipboard.
+-->
+
+<script lang="ts">
+
+import { base } from "$app/paths";
+
+
+interface Props {
+  value: string | undefined;
+}
+
+let { value }: Props = $props();
+
+
+let copied = $state(false);
+
+</script>
+
+
+<button class="copy"
+  onclick={() => {
+    if (value) {
+      navigator.clipboard.writeText(value);
+      copied = true;
+    }
+  }}
+>
+  {#if copied}
+    <img src="{base}/tick-new.svg" />
+  {:else}
+    <img src="{base}/copy.svg" />
+  {/if}
+</button>
+
+
+<style lang="scss">
+
+button.copy {
+  padding: 0.5em;
+  display: inline-block;
+  background: none;
+  border: none;
+  border-radius: 0.5em;
+  opacity: 40%;
+
+  @include interact($col-hover, $col-click);
+  @include focus-outline;
+}
+
+img {
+  width: 1.5em;
+  max-width: 100%;
+}
+
+</style>
