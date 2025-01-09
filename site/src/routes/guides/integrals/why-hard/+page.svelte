e1a209cdbcf162120613d70ac2adb966bb24111a
Sup#2.0 <sup2point0@gmail.com>
2025-01-09 17:23:18 +0000

    update guides

---
+++ site/src/routes/guides/integrals/why-hard/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+import Content from "./content.svx";
+import { metadata as page } from "./content.svx";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Meta title={page.title} desc={page.desc} />
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "Integration", intern: "guides/integrals" },
+  { text: "Why Is Integration Difficult?" },
+]} />
+
+<Header title="Why Is Integration Difficult?" capt="You can learn to differente in a day, but you can’t learn to integrate in a lifetime." />
+
+<article>
+  <Content />
+</article>
