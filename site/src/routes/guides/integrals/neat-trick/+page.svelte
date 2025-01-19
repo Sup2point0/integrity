93954bb03b2654d6407a366cc7552f0aca734884
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2025-01-19 22:15:13 +0000

    update guides

---
+++ site/src/routes/guides/integrals/neat-trick/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+
+import Content from "./content.svx";
+import { metadata as page } from "./content.svx";
+
+import ArticleCard from "#parts/ui/card.article.svelte";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Meta title={page.title} desc={page.desc} />
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "Integration", intern: "guides/integrals" },
+  { text: "Neat Integration Trick" },
+]} />
+
+<Header title="A Neat Little Integration Trick" capt={page.capt} />
+
+<article>
+  <Content />
+  
+  <br>
+
+  <h2> Continue Reading </h2>
+
+  // TODO
+</article>
