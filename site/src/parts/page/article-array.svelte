51b0332d2130affb8fda4048f458702ded8c63e8
Sup#2.0 <sup2point0@gmail.com>
2025-01-21 10:07:14 +0000

    add `#parts/article-array.svelte`

---
+++ site/src/parts/page/article-array.svelte
@@ -0,0 +1,36 @@
+<!-- @component ArticleArray
+
+Displays a list of article cards.
+-->
+
+<script lang="ts">
+
+import ArticleCard from "#parts/ui/card.article.svelte";
+
+
+interface Props {
+  paths: string[];
+}
+
+let { paths }: Props = $props();
+
+</script>
+
+
+<ul>
+  {#each paths as path}
+    <ArticleCard page={path} />
+  {/each}
+</ul>
+
+
+<style lang="scss">
+
+ul {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  gap: 1rem;
+}
+
+</style>
