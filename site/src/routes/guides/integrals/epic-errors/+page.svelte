c5b2c7309f988cc4d1aa94cb569793bc30981dbc
Sup#2.0 <sup2point0@gmail.com>
2025-01-21 09:30:38 +0000

    update guides

---
+++ site/src/routes/guides/integrals/epic-errors/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+
+import Content from "./content.svx";
+import { metadata as page } from "./content.svx";
+
+import ArticleCard from "#parts/ui/card.article.svelte";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Meta title={page.title} desc={page.desc} />
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "Integration", intern: "guides/integrals" },
+  { text: "Epic Errors" },
+]} />
+
+<Header title="Epic Errors" capt={page.capt} />
+
+<article>
+  <Content />
+  
+  <br>
+
+  <h2> Continue Reading </h2>
+
+  <ArticleCard page="guides/integrals/why-hard.md" />
+  <ArticleCard page="guides/integrals/how.md" />
+</article>
