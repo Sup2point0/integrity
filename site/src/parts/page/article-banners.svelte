11f862368e0c8695c30264f42785e4ba44e4810b
Sup#2.0 <sup2point0@gmail.com>
2025-01-22 21:09:48 +0000

    add page banners

---
+++ site/src/parts/page/article-banners.svelte
@@ -0,0 +1,40 @@
+<!-- @component ArticleBanners
+
+Handles displaying banners at the top of an article page.
+-->
+
+<script>
+
+import Banner from "#parts/page/banner.svelte";
+
+
+let { page } = $props();
+
+</script>
+
+
+<section class="banners">
+  {#if page.flags?.includes("dev")}
+    <Banner title="Unerased Construction Lines">
+      This page is currently a <span>work in progress</span>, so some parts may be incomplete or unpresentable. Advance at your own peril...
+    </Banner>
+  {/if}
+
+  {#if page.flags?.includes("opin")}
+    <Banner title="Objectivity">
+      This piece contains opinionated content, please do not feel obliged to agree.
+    </Banner>
+  {/if}
+</section>
+
+
+<style lang="scss">
+
+section.banners {
+  margin: 2rem 0;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1rem;
+}
+
+</style>
---
+++ site/src/parts/page/banner.svelte
@@ -0,0 +1,52 @@
+<!-- @component Banner
+
+A banner displayed at the top of a page below the header.
+-->
+
+<script lang="ts">
+
+interface Props {
+  title: string;
+  children?: any;
+}
+
+let { title, children }: Props = $props();
+
+</script>
+
+
+<aside class="banner">
+  <h3> {title} </h3>
+  <p> {@render children?.()} </p>
+</aside>
+
+
+<style lang="scss">
+
+aside {
+  padding: 0.25em 1.5em 0.4em;
+  border-left: 2px solid $col-prot;
+  transition: box-shadow 0.12s ease-out;
+
+  &:hover {
+    border-left: 2px solid $col-deut;
+    box-shadow: 1px 0 4px $col-line;
+  }
+}
+
+h3 {
+  padding-bottom: 0.25em;
+  @include font-serif;
+  color: $col-text;
+  font-weight: normal;
+  font-size: 150%;
+  font-style: italic;
+}
+
+p {
+  @include font-serif;
+  color: $col-text-deut;
+  font-size: 100%;
+}
+
+</style>
