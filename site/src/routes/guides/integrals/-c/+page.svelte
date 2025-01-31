8038ded765013c222e59dabaa3081a70a9f2e41a
Sup#2.0 <sup2point0@gmail.com>
2025-01-31 15:17:29 +0000

    update guides

---
+++ site/src/routes/guides/integrals/-c/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+import Content from "./~content.svx";
+import { metadata as page } from "./~content.svx";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+
+</script>
+
+
+<Meta title={page.title} desc={page.desc} />
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "Integration", intern: "guides/integrals" },
+  { text: page.title },
+]} />
+
+<Header page={page} />
+
+<article>
+  <Content />
+</article>
