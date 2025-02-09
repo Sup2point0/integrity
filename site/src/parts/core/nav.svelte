80add62d0a0c50d2d9064c811871f31bfaf6873e
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2025-02-09 14:09:19 +0000

    add fallback styles

--- site/src/parts/core/nav.svelte
+++ site/src/parts/core/nav.svelte
@@ -91,7 +91,9 @@ nav {
   flex-wrap: nowrap;
   gap: 0.5rem;
   background-color: $col-nav;
+  border-bottom: 1px solid #dedede;  // fallback
   border-bottom: 1px solid $col-line;
+  box-shadow: 0 2px 2px -1px #dedede;  // fallback
   box-shadow: 0 2px 2px -1px $col-line;
 
   &::before {
