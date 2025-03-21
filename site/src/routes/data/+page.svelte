9ee25a67c80803cc651548162f41aaab2aa73de9
Sup#2.0 <sup2point0@gmail.com>
2025-03-21 11:04:04 +0000

    add `data/`

---
+++ site/src/routes/data/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+
+import Site from "#src/scripts/site";
+import { userprefs } from "#scripts/stores";
+
+import ProgressBar from "./progress-bar.svelte";
+
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+import Meta from "#parts/page/meta.svelte";
+
+
+const total_questions = Site.get_list_of_all_questions().length;
+
+</script>
+
+
+<Meta title="Data"
+  desc="Your personal statistics on Integrity"
+/>
+
+
+<Breadcrumbs levels={[
+  { text: "Integrity", link: Site.root },
+  { text: "Data" },
+]} />
+
+
+<Header title="Data" capt="Your personal statistics" />
+
+<main>
+  <section>
+    <div class="row">
+      <p class="label"> Questions Solved </p>
+      <p class="count"> <span>{$userprefs.solved.size}</span> of {total_questions} </p>
+    </div>
+    <ProgressBar value={$userprefs.solved.size / total_questions} />
+  </section>
+  
+  <section>
+    <div class="row">
+      <p class="label"> Questions Starred </p>
+      <p class="count"> <span>{$userprefs.starred.size}</span> of {total_questions} </p>
+    </div>
+    <ProgressBar value={$userprefs.starred.size / total_questions} />
+  </section>
+  
+  <section>
+    <div class="row">
+      <p class="label"> Questions Flagged </p>
+      <p class="count"> <span>{$userprefs.flagged.size}</span> of {total_questions} </p>
+    </div>
+    <ProgressBar value={$userprefs.flagged.size / total_questions} />
+  </section>
+</main>
+
+
+<style lang="scss">
+
+main {
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+}
+
+section {
+  width: 80%;
+  padding: 1rem 0 2rem;
+}
+
+.row {
+  padding: 0 0 1rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  p {
+    font-size: 120%;
+
+    &.count {
+      color: $col-text-deut;
+    }
+
+    span {
+      font-weight: 400;
+      color: $col-prot;
+    }
+  }
+}
+
+</style>
---
+++ site/src/routes/data/progress-bar.svelte
@@ -0,0 +1,40 @@
+<!-- @component ProgressBar
+
+-->
+
+<script lang="ts">
+
+interface Props {
+  value: number;
+  total?: number;
+}
+
+let { value, total = 1 }: Props = $props();
+
+
+let frac = $derived(value / total);
+
+</script>
+
+
+<div class="progress-bar">
+  <div class="juice" style:width="{Math.round(100 * frac)}%"></div>
+</div>
+
+
+<style lang="scss">
+
+.progress-bar {
+  width: 100%;
+  height: 1.5rem;
+  background: $col-hover;
+  transform: skew(-20deg);
+}
+
+.juice {
+  height: 1.5rem;
+  background: $col-prot;
+  transition: width 0.1s ease-out;
+}
+
+</style>
