3838fadb42e75d2850da1bddd869c2dc7cd70cf2
Sup#2.0 <sup2point0@gmail.com>
2025-04-05 10:36:30 +0100

    add `#parts/question-array.svelte`

---
+++ site/src/parts/page/question-array.svelte
@@ -0,0 +1,42 @@
+<!-- @component QuestionArray
+
+Displays a list of question cards.
+-->
+
+<script lang="ts">
+
+import Site from "#scripts/site";
+
+import QuestionCard from "#parts/ui/card.question.svelte";
+
+
+interface Props {
+  shards: string[];
+}
+
+let { shards }: Props = $props();
+
+</script>
+
+
+<ul>
+  {#each shards as shard}
+    {@const question = Site.questions.integrals.questions[shard]}
+
+    <QuestionCard {question}
+      latex={question.question.content}
+      style="row"
+    />
+  {/each}
+</ul>
+
+
+<style lang="scss">
+
+ul {
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 1rem;
+}
+
+</style>
