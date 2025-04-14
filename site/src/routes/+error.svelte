144bc19ce5c5779993b874aee83d1110a8a99ed2
Sup#2.0 <sup2point0@gmail.com>
2025-04-14 10:50:56 +0100

    update error page to use `$app/state`

--- site/src/routes/+error.svelte
+++ site/src/routes/+error.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 
-import { page } from "$app/stores";
+import { page } from "$app/state";
 
 import Header from "#parts/core/header.svelte";
 
@@ -8,8 +8,8 @@ import Header from "#parts/core/header.svelte";
 
 
 <Header
-  title={$page.status ? `Error ${$page.status}` : "Internal Error"}
-  capt={$page.error?.message ?? "Something went wrong!"}
+  title={page.status ? `Error ${page.status}` : "Internal Error"}
+  capt={page.error?.message ?? "Something went wrong!"}
 />
 
 <p style:text-align="center"> If a link sent you here, I’m probably still working on this page! </p>
