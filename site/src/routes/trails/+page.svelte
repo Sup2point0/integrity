551ad0a9ed151e7c48098be2e5765689cb404d25
Sup#2.0 <sup2point0@gmail.com>
2025-06-19 09:11:33 +0100

    restructure trails page

--- site/src/routes/trails/+page.svelte
+++ site/src/routes/trails/+page.svelte
@@ -10,6 +10,7 @@ import Meta from "#parts/page/meta.svelte";
 import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
 import Header from "#parts/core/header.svelte";
 import Banner from "#parts/page/banner.svelte";
+import Section from "#src/parts/page/section.svelte";
 
 
 // temporary static data until we can get the endpoint working
@@ -21,7 +22,7 @@ let data = {
       March: null,
       April: 183,
       May: 58,
-      June: 61,
+      June: 80,
       July: 0,
       August: 0,
     },
@@ -31,25 +32,48 @@ let data = {
 let year: 2025 = $state(2025);
 let history = $derived(Object.entries(data.history[year] || {}));
 
-let integral_title_words = {};
-let integral_tags_counts = {};
+let integrals: {
+  title_words: Record<number, number>,
+  question_chars: Record<number | string, number>,
+  tags_counts: Record<number, number>,
+} = {
+  title_words: {},
+  question_chars: {},
+  tags_counts: {},
+};
 
 for (let q of Site.get_questions_of_topic("integrals")) {
   if (q.title) {
-    let word_count = q.title.split(" ").length;
-    if (integral_title_words[word_count]) {
-      integral_title_words[word_count]++;
+    let count = q.title.split(" ").length;
+
+    if (integrals.title_words[count]) {
+      integrals.title_words[count]++;
     } else {
-      integral_title_words[word_count] = 1;
+      integrals.title_words[count] = 1;
+    }
+  }
+
+  if (q.question) {
+    let raw = q.question.content.length;
+    let count = Math.round(raw / 5) * 5;
+    if (count > 200) {
+      count = "200+";
+    }
+
+    if (integrals.question_chars[count]) {
+      integrals.question_chars[count]++;
+    } else {
+      integrals.question_chars[count] = 1;
     }
   }
 
   if (q.tags) {
-    let tags_count = q.tags.length;
-    if (integral_tags_counts[tags_count]) {
-      integral_tags_counts[tags_count]++;
+    let count = q.tags.length;
+
+    if (integrals.tags_counts[count]) {
+      integrals.tags_counts[count]++;
     } else {
-      integral_tags_counts[tags_count] = 1;
+      integrals.tags_counts[count] = 1;
     }
   }
 }
@@ -57,7 +81,7 @@ for (let q of Site.get_questions_of_topic("integrals")) {
 </script>
 
 
-<Meta title="Trails" desc="A historic count of visits to Integrity" />
+<Meta title="Trails" desc="Historic visitor count and other fun statistics for Integrity" />
 
 
 <Breadcrumbs levels={[
@@ -65,23 +89,25 @@ for (let q of Site.get_questions_of_topic("integrals")) {
   { text: "Trails" },
 ]} />
 
-<Header title="Trails" capt="Marks of past adventurers" />
-
-<Banner title="Mysterious Beginnings">
-  <em>Integrity</em> only started counting visits in April 2025, so data before this point is unavailable!
-</Banner>
+<Header title="Trails" capt="Statistics of Integrity" />
 
 
-<nav>
-  <small> View Year </small>
-  <Select bind:value={year} options={[ 2025 ]} />
-</nav>
+<Section title="Visits" closed={false}>
+  <h2> Visits to <em>Integrity</em> </h2>
+  
+  <Banner title="Mysterious Beginnings">
+    <em>Integrity</em> only started counting visits in April 2025, so data before this point is unavailable!
+  </Banner>
 
-<div class="container">
+  <nav>
+    <small> View Year </small>
+    <Select bind:value={year} options={[ 2025 ]} />
+  </nav>
+
   {#if data}
     {@const highest = Math.max(...history.map(([_, count]) => count ?? 0))}
 
-    <div id="chart">
+    <div class="chart">
       {#each history as [month, count]}
         <div class="column">
           <div class="freq-label">
@@ -96,59 +122,57 @@ for (let q of Site.get_questions_of_topic("integrals")) {
         </div>
       {/each}
     </div>
+
+    <Section title="Stats" closed={true}>
+      {#if data}
+        <!-- heh, who doesn't love statistics. Even Timothee Chalamet loves sTaTiSTiCs -->
+        {@const   n = history.reduce((t, [_, count])    => t + (count ? 1 : 0),             0)}
+        {@const nfx = history.reduce((t, [_, count], i) => t + (count ? (i+1) : 0),         0)}
+        {@const  sx = history.reduce((t, [_, count], i) => t + (count ? i+1 : 0),           0)}
+        {@const sxs = history.reduce((t, [_, count], i) => t + (count ? (i+1)**2 : 0),      0)}
+        {@const  sf = history.reduce((t, [_, count])    => t + (count ?? 0),                0)}
+        {@const sfs = history.reduce((t, [_, count])    => t + (count ? count**2 : 0),      0)}
+        {@const sfx = history.reduce((t, [_, count], i) => t + (count ? (i+1) * count : 0), 0)}
+
+        <!-- NOTE: I apologise for your eyes. Offsetting this way so that the LaTeX structure is still visible. Couldn't find a cleaner way to do the string interpolation... -->
+        {@html katex.renderToString(
+            String.raw`
+                        \begin{align*}
+                          && n &= ` + n.toString()
+          + String.raw`
+                          \\[1em] \sum x &= ` + sx.toString()
+          + String.raw`
+                            \quad&\quad \sum f &= ` + sf.toString()
+          + String.raw`
+                            \quad&\quad \sum fx &= ` + sfx.toString()
+          + String.raw`
+                          \\[1em] \sum x^2 &= ` + sxs.toString()
+          + String.raw`
+                            \quad&\quad  \sum f^2 &= ` + sfs.toString()
+          + String.raw`
+                          \\[2em] \bar{x} &= \frac{\sum fx}{\sum x} = ` + (sfx / sf).toFixed(2)
+          + String.raw`
+                            \quad&\quad \bar{f} &= \frac{1}{n}\sum f = ` + (sf / n).toFixed(2)
+          + String.raw`
+                            \quad&\quad \sigma^2_f &= \text{Var}(F) = ` + (sfs / n - (sf / n)**2).toFixed(2)
+          + String.raw`
+                        \end{align*}
+          `,
+          { displayMode: true, throwOnError: false }
+        )}
+      {/if}
+    </Section>
   {/if}
-</div>
+</Section>
 
-<div class="stats">
-  {#if data}
-    <!-- heh, who doesn't love statistics. Even Timothee Chalamet loves sTaTiSTiCs -->
-    {@const   n = history.reduce((t, [_, count])    => t + (count ? 1 : 0),             0)}
-    {@const nfx = history.reduce((t, [_, count], i) => t + (count ? (i+1) : 0),         0)}
-    {@const  sx = history.reduce((t, [_, count], i) => t + (count ? i+1 : 0),           0)}
-    {@const sxs = history.reduce((t, [_, count], i) => t + (count ? (i+1)**2 : 0),      0)}
-    {@const  sf = history.reduce((t, [_, count])    => t + (count ?? 0),                0)}
-    {@const sfs = history.reduce((t, [_, count])    => t + (count ? count**2 : 0),      0)}
-    {@const sfx = history.reduce((t, [_, count], i) => t + (count ? (i+1) * count : 0), 0)}
+<Section title="Integrals" closed={false}>
+  <h2> Number of Words in Title </h2>
 
-    <!-- NOTE: I apologise for your eyes. Offsetting this way so that the LaTeX structure is still visible. Couldn't find a cleaner way to do the string interpolation... -->
-    {@html katex.renderToString(
-        String.raw`
-                    \begin{align*}
-                      && n &= ` + n.toString()
-      + String.raw`
-                      \\[1em] \sum x &= ` + sx.toString()
-      + String.raw`
-                        \quad&\quad \sum f &= ` + sf.toString()
-      + String.raw`
-                        \quad&\quad \sum fx &= ` + sfx.toString()
-      + String.raw`
-                      \\[1em] \sum x^2 &= ` + sxs.toString()
-      + String.raw`
-                        \quad&\quad  \sum f^2 &= ` + sfs.toString()
-      + String.raw`
-                      \\[2em] \bar{x} &= \frac{\sum fx}{\sum x} = ` + (sfx / sf).toFixed(2)
-      + String.raw`
-                        \quad&\quad \bar{f} &= \frac{1}{n}\sum f = ` + (sf / n).toFixed(2)
-      + String.raw`
-                        \quad&\quad \sigma^2_f &= \text{Var}(F) = ` + (sfs / n - (sf / n)**2).toFixed(2)
-      + String.raw`
-                    \end{align*}
-      `,
-      { displayMode: true, throwOnError: false }
-    )}
-  {/if}
-</div>
+  {#if integrals.title_words}
+    {@const highest = Math.max(...Object.values(integrals.title_words))}
 
-<Header title="Fun" />
-
-<div class="container">
-  <h2> Integrals by Number of Words in Title </h2>
-
-  {#if integral_title_words}
-    {@const highest = Math.max(...Object.values(integral_title_words))}
-
-    <div id="chart">
-      {#each Object.entries(integral_title_words) as [count, freq]}
+    <div class="chart">
+      {#each Object.entries(integrals.title_words) as [count, freq]}
         <div class="column">
           <div class="freq-label">
             {freq ?? "No data"}
@@ -163,16 +187,40 @@ for (let q of Site.get_questions_of_topic("integrals")) {
       {/each}
     </div>
   {/if}
-</div>
 
-<div class="container">
-  <h2> Integrals by Number of Tags </h2>
+  <h2> Characters in Question LaTeX </h2>
 
-  {#if integral_tags_counts}
-    {@const highest = Math.max(...Object.values(integral_tags_counts))}
+  {#if integrals.question_chars}
+    {@const highest = Math.max(...Object.values(integrals.question_chars))}
 
-    <div id="chart">
-      {#each Object.entries(integral_tags_counts) as [count, freq]}
+    <div class="chart">
+      {#each Object.entries(integrals.question_chars) as [count, freq]}
+        <div class="column">
+          <div class="freq-label">
+            {freq ?? "No data"}
+          </div>
+
+          <div class="bar" style:--frac={(freq ?? 0) / highest}></div>
+
+          <div class="class-label" style:font-size="75%">
+            {#if count.endsWith("+")}
+              {count}
+            {:else}
+              &lt;{count}
+            {/if}
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
+
+  <h2> Number of Tags </h2>
+
+  {#if integrals.tags_counts}
+    {@const highest = Math.max(...Object.values(integrals.tags_counts))}
+
+    <div class="chart">
+      {#each Object.entries(integrals.tags_counts) as [count, freq]}
         <div class="column">
           <div class="freq-label">
             {freq ?? "No data"}
@@ -187,7 +235,7 @@ for (let q of Site.get_questions_of_topic("integrals")) {
       {/each}
     </div>
   {/if}
-</div>
+</Section>
 
 
 <style lang="scss">
@@ -205,14 +253,10 @@ nav {
   }
 }
 
-.container {
+.chart {
   height: 60vh;
-  margin-bottom: 4rem;
-}
-
-#chart {
-  height: 100%;
-  padding: 1rem;
+  padding: 1rem 0.5rem;
+  margin-bottom: 5rem;
   display: flex;
   flex-flow: row nowrap;
   border-left: 1px solid $col-line;
@@ -269,14 +313,10 @@ nav {
   }
 }
 
-.stats {
-  padding: 2rem 0;
-}
-
 h2 {
   @include font-serif;
   font-weight: 400;
-  font-size: 150%;
+  font-size: 175%;
   text-align: center;
 }
 
