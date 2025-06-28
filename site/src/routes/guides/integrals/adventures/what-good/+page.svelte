a7c0aee59e0b49e7605b77453e2592bcc1eb04e0
Sup#2.0 <sup2point0@gmail.com>
2025-06-28 18:13:09 +0100

    add article

---
+++ site/src/routes/guides/integrals/adventures/what-good/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+import Content from "./~content.svx";
+import { metadata as page } from "./~content.svx";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+import ArticleArray from "#parts/page/article-array.svelte";
+
+</script>
+
+
+<Meta title={page.title} desc={page.desc} />
+
+
+<Breadcrumbs levels={[
+  { text: "Guides", intern: "guides" },
+  { text: "Integration", intern: "guides/integrals" },
+  { text: "Adventures", intern: "guides/integrals#adventures" },
+  { text: page.title },
+]} />
+
+<Header {page} />
+
+<article>
+  <Content />
+
+  <footer>
+    <p> Last updated <span>{page.date_display}</span> </p>
+  </footer>
+
+  <aside>
+    <h2> Continue Reading </h2>
+    
+    <ArticleArray paths={[
+      "guides/integrals/why-hard.md",
+    ]} />
+  </aside>
+</article>
