1c4b5ada014fc5abea33aef643bdfc63ad4dfe1e
Sup#2.0 <sup2point0@gmail.com>
2025-07-05 20:20:32 +0100

    allow multiple runs of preprocessing scripts

--- .github/scripts/run.rb
+++ .github/scripts/run.rb
@@ -6,37 +6,42 @@ require "pathname"
 
 require_relative "process"
 
-## find
+
+def execute(source, dest)
+  total = source.length
+
+  data = {}
+  i = 1
+
+  source.each do |file|
+    log "#{i}#{GREY} of #{total}: #{WHITE}#{file.parent.basename}#{GREY}/#{BLUE}#{file.basename}"
+    
+    shard = file.basename(".*")
+
+    out = process(shard:, file:)
+    topic = out["topic"]
+    if topic.nil? then next end
+
+    if data[topic].nil? then data[topic] = {} end
+    data[topic][shard] = out
+
+    i += 1
+  end
+
+  out = JSON.pretty_generate(data)
+  File.write(dest, out)
+end
+
+
+## run
 log "finding question files..."
 
 ROOT = Pathname(__dir__).parent.parent
 
-SOURCE = ROOT / "questions"
-files = SOURCE.glob("**/*-*.md")
-total = files.length
+questions = (ROOT/"questions").glob("**/*-*.md")
+log success: "found #{questions.length} question files!"
+execute(questions, ROOT/"site/src/data/questions.json")
 
-log success: "found #{total} question files!"
-
-## process
-data = {}
-i = 1
-
-files.each do |file|
-  log "#{i}#{GREY} of #{total}: #{WHITE}#{file.parent.basename}#{GREY}/#{BLUE}#{file.basename}"
-  
-  shard = file.basename(".*")
-
-  out = process(shard:, file:)
-  topic = out["topic"]
-  if topic.nil? then next end
-
-  if data[topic].nil? then data[topic] = {} end
-  data[topic][shard] = out
-
-  i += 1
-end
-
-## save
-out = JSON.pretty_generate(data)
-dest = ROOT / "site/src/data/questions.json"
-File.write(dest, out)
+scriptures = (ROOT/"scriptures/desmos/gamedev").glob("**/**.md")
+log success: "found #{scriptures.length} scripture files!"
+execute(scriptures, ROOT/"site/src/data/scriptures.json")
