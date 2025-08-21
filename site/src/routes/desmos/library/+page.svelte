4f317932c48ad7b0fe7f6f77fb89bcd4163dcd33
Sup#2.0 <sup2point0@gmail.com>
2025-08-21 20:55:20 +0800

    update desmos library

---
+++ site/src/routes/desmos/library/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+
+import Card from "#parts/ui/card.svelte";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+import ArticleArray from "#parts/page/article-array.svelte";
+
+</script>
+
+
+<Meta title="Library" desc="Standard library of " />
+
+
+<Breadcrumbs levels={[
+  { text: "Desmos", intern: "desmos" },
+  { text: "Library" }
+]} />
+
+<Header title="Library" />
+
+
+<div class="content">
+  <Card title="Functions"
+    intern="desmos/library/functions"
+    latex={String.raw`f_\text{activate}(s) = \{ s > 0 : 1, 0 \}`}
+  />
+  <Card title="Rendering"
+    intern="desmos/library/rendering"
+    latex={String.raw`d_\text{polygon}(6, (0, 0), 2, \pi/3)`}
+  />
+</div>
+
+
+<style lang="scss">
+
+.content {
+  margin: 1rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  gap: 1rem;
+  text-align: center;
+}
+
+</style>
---
+++ site/src/routes/desmos/library/functions/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+import Site from "#src/scripts/site";
+
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+import Meta from "#parts/page/meta.svelte";
+import ArticleArray from "#parts/page/article-array.svelte";
+
+</script>
+
+
+<Meta title="Functions"
+  desc="Desmos standard library functions"
+/>
+
+
+<Breadcrumbs levels={[
+  { text: "Desmos", intern: "desmos" },
+  { text: "Library", intern: "desmos/library" },
+  { text: "Functions" },
+]} />
+
+<Header title="Functions"
+  capt="Standard Library"
+/>
+
+
+<ArticleArray
+  paths={Site.index.library.pages.filter(path => (
+    Site.index.functions.pages.includes(path)
+  ))}
+/>
