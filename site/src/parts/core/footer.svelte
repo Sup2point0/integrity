3ef23f9c350b560a88281b085f610314a6a956ab
Sup#2.0 <sup2point0@gmail.com>
2025-09-15 13:41:04 +0100

    update footer

--- site/src/parts/core/footer.svelte
+++ site/src/parts/core/footer.svelte
@@ -14,7 +14,7 @@ import { base } from "$app/paths";
 
 
 <footer>
-  <div class="content">
+  <div class="upper">
 
     <div class="left">
       <img id="integrity-title"
@@ -45,16 +45,24 @@ import { base } from "$app/paths";
         />
       </a>
 
-      <a class="credits" target="_blank" href="https://github.com/Sup2point0/integrity">
-        <p> View on GitHub </p>
-      </a>
+      <div class="outlinks">
+        <a target="_blank" href="https://github.com/Sup2point0/integrity">
+          <p> View on GitHub <code>↗</code> </p>
+        </a>
+
+        <a target="_blank" href="https://github.com/Sup2point0/integrity/issues">
+          <p> Report Issue <code>↗</code> </p>
+        </a>
+      </div>
     </div>
 
   </div>
 
-  <small class="centre">
-    built with <a target="_blank" href="https://svelte.dev/">Svelte 5</a> + <a target="_blank" href="https://svelte.dev/docs/kit/introduction">SvelteKit</a> &ensp; · &ensp; hosted on <a target="_blank" href="https://pages.github.com/">GitHub Pages</a>
-  </small>
+  <div class="lower">
+    <small>
+      built with <a target="_blank" href="https://svelte.dev/">Svelte 5</a> + <a target="_blank" href="https://svelte.dev/docs/kit/introduction">SvelteKit</a> &ensp; · &ensp; hosted on <a target="_blank" href="https://pages.github.com/">GitHub Pages</a>
+    </small>
+  </div>
 </footer>
 
 
@@ -64,98 +72,113 @@ footer {
   width: 100%;
   padding: 4rem 0 2rem;
   background: light-dark(oklch(99% 0 0), black);
-
-  .content {
-    width: 80%;
-    max-width: 80rem;
-    height: 100%;
-    margin: 0 auto;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-items: start;
-    gap: 2rem;
-  }
 }
 
-.quicklinks {
+
+.upper {
+  width: 80%;
+  max-width: 80rem;
+  height: 100%;
+  margin: 0 auto;
   display: flex;
   flex-flow: row wrap;
-  gap: 0.25em;
+  align-items: start;
+  gap: 2rem;
 }
 
-small {
-  margin-top: 4rem;
-  display: block;
-  @include font-ui;
-  font-size: 85%;
-  color: $col-text-deut;
+.left {
+  img#integrity-title {
+    min-width: 10rem;
+    max-width: 15vw;
+    margin-bottom: 1rem;
 
-  a {
-    @include underline-link;
+    @media (max-width: 40rem) {
+      max-width: 50vw;
+    }
+  }
+
+  .quicklinks {
+    display: flex;
+    flex-flow: row wrap;
+    gap: 0.25em;
   }
 }
 
-
-.left {
-  text-align: left;
-}
 .right {
+  margin-left: auto;
   display: flex;
   flex-flow: column;
   align-items: end;
   text-align: right;
+
+  a {
+    width: max-content;
+    padding: 1em;
+    display: flex;
+    flex-flow: row;
+    column-gap: 1rem;
+    color: $col-text;
+    text-align: right;
+    text-decoration: none;
+    border-radius: 1em;
+
+    @include interact();
+    @include focus-outline;
+
+    p {
+      width: 100%;
+      display: block;
+    }
+
+    &:hover img#purple-portal {
+      transform: rotate(80deg);
+    }
+  }
+
+  img#purple-portal {
+    max-height: 4em;
+    aspect-ratio: 1;
+    transition: transform 2s cubic-bezier(0.19, 1, 0.22, 1);  // ease-out-exp
+  }
+
+  p#sup2point0 {
+    padding-top: 0.2em;
+    color: $col-prot-fallback;
+    color: $col-prot;
+    font-size: 200%;
+  }
+
+  .outlinks {
+    display: flex;
+    flex-flow: row wrap;
+
+    a {
+      padding: 0.5em 1em;
+      font-size: 90%;
+      border-radius: 0.5em;
+      opacity: 40%;
+
+      &:hover, &:focus, &:focus-visible {
+        opacity: 100%;
+      }
+    }
+  }
 }
-.centre {
+
+
+.lower {
+  padding-top: 5rem;
   text-align: center;
-}
 
+  small {
+    @include font-ui;
+    font-size: 85%;
+    color: $col-text-deut;
 
-a.credits {
-  width: max-content;
-  padding: 1em;
-  display: flex;
-  flex-flow: row;
-  column-gap: 1rem;
-  color: $col-text;
-  text-align: right;
-  text-decoration: none;
-  border-radius: 1em;
-
-  @include interact();
-  @include focus-outline;
-
-  p {
-    width: 100%;
-    display: block;
+    a {
+      @include underline-link;
+    }
   }
-
-  &:hover img#purple-portal {
-    transform: rotate(80deg);
-  }
-}
-
-
-img#integrity-title {
-  max-width: 15vw;
-  margin-bottom: 1rem;
-
-  @media (max-width: 40rem) {
-    max-width: 50vw;
-  }
-}
-
-img#purple-portal {
-  max-height: 4em;
-  aspect-ratio: 1;
-  transition: transform 0.5s ease-out;
-}
-
-p#sup2point0 {
-  padding-top: 0.2em;
-  color: $col-prot-fallback;
-  color: $col-prot;
-  font-size: 200%;
 }
 
 </style>
