001dab6cdc887e43ce2a2ffaa6d07e2c1dc06ff0
Sup#2.0 <sup2point0@gmail.com>
2025-09-18 15:37:16 +0100

    add graph drawing scriptures index page

---
+++ site/src/routes/scriptures/graph-drawing/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+
+import Site from "#scripts/site";
+
+import ArticleArray from "#parts/page/article-array.svelte";
+
+import Meta from "#parts/page/meta.svelte";
+import Breadcrumbs from "#parts/page/breadcrumbs.svelte";
+import Header from "#parts/core/header.svelte";
+import Line from "#parts/page/line.svelte";
+
+</script>
+
+
+<Meta title="Graph Drawing · Scriptures" />
+
+
+<Breadcrumbs levels={[
+  { text: "Scriptures", intern: "scriptures" },
+  { text: "Graph Drawing" }
+]} />
+
+<Header title="Graph Drawing" />
+
+
+<Line />
+
+<section>
+  <!-- TODO -->
+</section>
+
+<section>
+  <h2 id="antiderivatives"> Dynamic Scriptures </h2>
+  <ArticleArray pages={[
+    { dest: "scriptures/graph-drawing/dyna/multiplication", head: "Multiplication as Scaling" },
+  ]} />
+</section>
+
+
+<style lang="scss">
+
+section {
+  margin: 0 0 4rem;
+  text-align: center;
+}
+
+h2 {
+  margin: 1em 0;
+  padding: 0 0.1em 0.25em;
+  @include font-serif;
+  font-weight: 400;
+  font-size: 200%;
+}
+
+</style>
