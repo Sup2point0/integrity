1f4d8edad9deeca742adffe5a38777711dec6fc7
Sup#2.0 <sup2point0@gmail.com>
2026-08-22 13:16:15 +0100

    migrate `<DynamicScripture>` to desmost

--- site/src/parts/base/dynamic-scripture.svelte
+++ site/src/parts/base/dynamic-scripture.svelte
@@ -6,29 +6,28 @@ The fully implemented dynamic scripture viewer.
 <script lang="ts">
 
 import { dyna_scriptures } from "#scripts/site";
-import type { Block, DynamicScripture } from "#scripts/types";
+import type { DynamicScripture } from "#scripts/types";
 
 import { DesmosAPI, Header, Meta } from "#parts/core";
 import { RenderBlock } from "#parts/page";
 import { Clicky, Desmos } from "#parts/ui";
 
 import { navigating, page } from "$app/state";
+import { goto, onNavigate } from "$app/navigation";
 import { untrack } from "svelte";
 import { expoOut } from "svelte/easing";
 import { fade, slide } from "svelte/transition";
-
-import { goto, onNavigate } from "$app/navigation";
 
 
 let data: DynamicScripture = $derived(page.data as DynamicScripture);
 let sections_list = $derived(Object.values(data.sections));
 
 let updated = $derived(
-  new Intl.DateTimeFormat("en-GB", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-  }
+	new Intl.DateTimeFormat("en-GB", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+	}
 ).format(data.update ?? undefined));
 
 
@@ -37,82 +36,74 @@ let current_section = $state(0);
 let current_subsection = $state(0);
 
 let next_disabled = $derived(
-  current_section === sections_list.length -1 &&
-  current_subsection === sections_list.at(-1)!.subsections.length -1
+	current_section === sections_list.length -1 &&
+	current_subsection === sections_list.at(-1)!.subsections.length -1
 );
 
 let shown_subsections = $derived(
-  sections_list[current_section]
-  .subsections.slice(0, current_subsection +1)
+	sections_list[current_section]
+	.subsections.slice(0, current_subsection +1)
 );
-let desmos_blocks: Block[] | null = $state(null);
+let desmos_source: string = $state("");
 
-/* svelte-ignore non_reactive_update */
-let article_viewport: HTMLElement | null = null;
+let article_viewport: HTMLElement | null = $state(null);
 
 
 $effect(() => {
-  current_section;
-  current_subsection;
+	current_section;
+	current_subsection;
 
-  untrack(() => {
-    let subsection = sections_list[current_section].subsections[current_subsection];
-    if (subsection[0].kind === "desmos") {
-      desmos_blocks = (subsection[0].content
-        .split("\n")
-        .map(line => ({
-          kind: "desmos",
-          content: line,
-        }))
-      );
-    }
-    else {
-      if (current_subsection === 0) {
-        desmos_blocks = null;
-      }
-    }
+	untrack(() => {
+		let subsection = sections_list[current_section].subsections[current_subsection];
 
-    article_viewport?.scrollTo({ top: article_viewport.scrollHeight, behavior: "smooth" });
-  });
+		if (subsection[0].kind === "desmos") {
+			desmos_source = subsection[0].content;
+		}
+		else if (current_subsection === 0) {
+			desmos_source = "";
+		}
+
+		article_viewport?.scrollTo({ top: article_viewport.scrollHeight, behavior: "smooth" });
+	});
 });
 
 $effect(() => {
-  page.url;
+	page.url;
 
-  untrack(() => {
-    started = false;
-    current_section = 0;
-    current_subsection = 0;
-  });
+	untrack(() => {
+		started = false;
+		current_section = 0;
+		current_subsection = 0;
+	});
 });
 
 onNavigate(async (navigation) => {
-  if (!document.startViewTransition) return;
+	if (!document.startViewTransition) return;
 
-  let url = navigating.to?.url.toString();
-  let parts = url?.split("/");
+	let url = navigating.to?.url.toString();
+	let parts = url?.split("/");
 
-  if (parts?.at(-4) === "desmos" || parts?.at(-3) === "gamedev") {
-    return new Promise(resolve => {
-      document.startViewTransition(async () => {
-        resolve();
-        await navigation.complete;
-      });
-    });
-  }
+	if (parts?.at(-4) === "desmos" || parts?.at(-3) === "gamedev") {
+		return new Promise(resolve => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	}
 });
 
 
 function next_subsection()
 {
-  if (next_disabled) return;
+	if (next_disabled) return;
 
-  if (sections_list[current_section].subsections.length === current_subsection +1) {
-    current_section++;
-    current_subsection = 0;
-  } else {
-    current_subsection++;
-  }
+	if (sections_list[current_section].subsections.length === current_subsection +1) {
+		current_section++;
+		current_subsection = 0;
+	} else {
+		current_subsection++;
+	}
 }
 
 </script>
@@ -122,127 +113,127 @@ function next_subsection()
 <DesmosAPI />
 
 <svelte:window onkeydown={e => {
-  if (!started) {
-    started = true;
-    return;
-  }
-  
-  if (e.key === "Enter" && e.shiftKey) {
-    next_subsection();
-  }
+	if (!started) {
+		started = true;
+		return;
+	}
+	
+	if (e.key === "Enter" && e.shiftKey) {
+		next_subsection();
+	}
 }} />
 
 
 {#if started}
-  <div in:fade={{ duration: 250, delay: 250 }}>
-    <Header title={data.title} capt="Last Updated {updated}" />
+	<div in:fade={{ duration: 250, delay: 250 }}>
+		<Header title={data.title} capt="Last Updated {updated}" />
 
-    <nav class="upper"
-      class:tight={data.sections.filter(sec => sec.subsections.length > 8).length > 1}
-    >
-      {#each data.sections as section, i}
-        {#each section.subsections as _, j}
-          <div class={["subsection", {
-            live: current_section === i && current_subsection === j,
-            done: current_section > i || (current_section === i && current_subsection > j),
-            edge: j === 0,
-          }]}>
-            <button class="juice" aria-label="subsection" onclick={() => {
-              current_section = i;
-              current_subsection = j;
-            }}>
-            </button>
-            
-            {#if j === 0}
-              <div class="section-label"
-                class:down={(data.sections.length > 3) && (i % 2 == 1)}
-              >
-                {section.title}
-              </div>
-            {/if}
-          </div>
-        {/each}
-      {/each}
-    </nav>
+		<nav class="upper"
+			class:tight={data.sections.filter(sec => sec.subsections.length > 8).length > 1}
+		>
+			{#each data.sections as section, i}
+				{#each section.subsections as _, j}
+					<div class={["subsection", {
+						live: current_section === i && current_subsection === j,
+						done: current_section > i || (current_section === i && current_subsection > j),
+						edge: j === 0,
+					}]}>
+						<button class="juice" aria-label="subsection" onclick={() => {
+							current_section = i;
+							current_subsection = j;
+						}}>
+						</button>
+						
+						{#if j === 0}
+							<div class="section-label"
+								class:down={(data.sections.length > 3) && (i % 2 == 1)}
+							>
+								{section.title}
+							</div>
+						{/if}
+					</div>
+				{/each}
+			{/each}
+		</nav>
 
-    <div class="layout">
-      <div class="half left">
-        <article bind:this={article_viewport}>
-          {#each shown_subsections as subsection, idx (subsection)}
-            <div class="padding-container"
-              in:fade={{ duration: 250 }}
-              out:slide={{ duration: 500, easing: expoOut }}
-            >
-              <!-- yes, it is a tad confusing to use <section> for each subsection, but... -->
-              <section class="sub dyna-scripture"
-                class:live={idx === shown_subsections.length -1}
-              >
-                {#each subsection as source}
-                  {#if source.kind !== "desmos"}
-                    <RenderBlock {source} />
-                  {/if}
-                {/each}
-              </section>
-            </div>
-          {/each}
-        </article>
-        
-        <nav class="lower">
-          <Clicky text="Previous"
-            action={() => {
-              if (current_subsection === 0) {
-                current_section--;
-                current_subsection = sections_list[current_section].subsections.length -1;
-              } else {
-                current_subsection--;
-              }
-            }}
-            disabled={current_section === 0 && current_subsection === 0}
-          />
+		<div class="layout">
+			<div class="half left">
+				<article bind:this={article_viewport}>
+					{#each shown_subsections as subsection, idx (subsection)}
+						<div class="padding-container"
+							in:fade={{ duration: 250 }}
+							out:slide={{ duration: 500, easing: expoOut }}
+						>
+							<!-- yes, it is a tad confusing to use <section> for each subsection, but... -->
+							<section class="sub dyna-scripture"
+								class:live={idx === shown_subsections.length -1}
+							>
+								{#each subsection as source}
+									{#if source.kind !== "desmos"}
+										<RenderBlock {source} />
+									{/if}
+								{/each}
+							</section>
+						</div>
+					{/each}
+				</article>
+				
+				<nav class="lower">
+					<Clicky text="Previous"
+						action={() => {
+							if (current_subsection === 0) {
+								current_section--;
+								current_subsection = sections_list[current_section].subsections.length -1;
+							} else {
+								current_subsection--;
+							}
+						}}
+						disabled={current_section === 0 && current_subsection === 0}
+					/>
 
-          {#if next_disabled && data.next}
-            <Clicky text="Next Up: {dyna_scriptures[data.chapter.toLowerCase()][data.next].title}"
-              action={() => goto(`/desmos/gamedev/${data.chapter.toLowerCase()}/${data.next}`)}
-            />
-          {:else}
-            <Clicky text="Next"
-              action={next_subsection}
-              disabled={next_disabled}
-            />
-          {/if}
-        </nav>
-      </div>
+					{#if next_disabled && data.next}
+						<Clicky text="Next Up: {dyna_scriptures[data.chapter.toLowerCase()][data.next].title}"
+							action={() => goto(`/desmos/gamedev/${data.chapter.toLowerCase()}/${data.next}`)}
+						/>
+					{:else}
+						<Clicky text="Next"
+							action={next_subsection}
+							disabled={next_disabled}
+						/>
+					{/if}
+				</nav>
+			</div>
 
-      <div class="half right">
-        {#key desmos_blocks}
-          <Desmos
-            blocks={desmos_blocks}
-            options={{ expressionsCollapsed: false }}
-            height="70vh"
-            no_delay={true}
-          />
-        {/key}
-      </div>
-    </div>
-  </div>
+			<div class="half right">
+				{#key desmos_source}
+					<Desmos
+						source={desmos_source}
+						options={{ expressionsCollapsed: false }}
+						height="70vh"
+						no_delay={true}
+					/>
+				{/key}
+			</div>
+		</div>
+	</div>
 
 {:else}
-  <aside class="overlay" out:fade={{ duration: 250 }}>
-    <header>
-      <h1> {data.title} </h1>
+	<aside class="overlay" out:fade={{ duration: 250 }}>
+		<header>
+			<h1> {data.title} </h1>
 
-      {#if data.desc}
-        <div class="desc">
-          <RenderBlock source={data.desc} />
-        </div>
-      {/if}
-    </header>
+			{#if data.desc}
+				<div class="desc">
+					<RenderBlock source={data.desc} />
+				</div>
+			{/if}
+		</header>
 
-    <nav class="lower">
-      <p> Last updated {updated} </p>
-      <Clicky text="Start" action={() => { started = true; }} />
-    </nav>
-  </aside>
+		<nav class="lower">
+			<p> Last updated {updated} </p>
+			<Clicky text="Start" action={() => { started = true; }} />
+		</nav>
+	</aside>
 
 {/if}
 
@@ -253,191 +244,191 @@ function next_subsection()
 
 
 nav {
-  width: 100%;
-  display: flex;
-  flex-flow: row nowrap;
-  gap: 0.4rem;
+	width: 100%;
+	display: flex;
+	flex-flow: row nowrap;
+	gap: 0.4rem;
 }
 
 nav.upper {
-  width: 100%;
-  padding: 2rem 0 3rem;
+	width: 100%;
+	padding: 2rem 0 3rem;
 
-  &.tight {
-    gap: 0.3rem;
-  }
+	&.tight {
+		gap: 0.3rem;
+	}
 
-  .subsection {
-    flex: 1 1 0;
-    height: 1.25rem;
-    position: relative;
-    transition: all 0.5s cubic-bezier(0.19, 1, 0.22, 1);  // ease-out-exp
-    
-    &.live {
-      pointer-events: none;
-      flex-grow: 2;
-    }
+	.subsection {
+		flex: 1 1 0;
+		height: 1.25rem;
+		position: relative;
+		transition: all 0.5s cubic-bezier(0.19, 1, 0.22, 1);  // ease-out-exp
+		
+		&.live {
+			pointer-events: none;
+			flex-grow: 2;
+		}
 
-    .juice {
-      width: 100%;
-      height: 100%;
-      background: light-dark(rgb(black, 4%), rgb(white, 12%));
-      border: none;
-      transform: skew(-20deg);
+		.juice {
+			width: 100%;
+			height: 100%;
+			background: light-dark(rgb(black, 4%), rgb(white, 12%));
+			border: none;
+			transform: skew(-20deg);
 
-      @include interact(
-        light-dark(rgb(black, 8%), rgb(white, 16%)),
-        light-dark(rgb(black, 12%), rgb(white, 20%)),
-      );
-      @include focus-outline;
-    }
+			@include interact(
+				light-dark(rgb(black, 8%), rgb(white, 16%)),
+				light-dark(rgb(black, 12%), rgb(white, 20%)),
+			);
+			@include focus-outline;
+		}
 
-    &.live .juice {
-      background: $col-deut;
-    }
+		&.live .juice {
+			background: $col-deut;
+		}
 
-    &.done .juice {
-      background: $col-prot;
+		&.done .juice {
+			background: $col-prot;
 
-      @include interact(
-        $hover: color-mix(in oklch, $col-prot, black 8%),
-        $click: color-mix(in oklch, $col-prot, black 16%),
-      );
-    }
+			@include interact(
+				$hover: color-mix(in oklch, $col-prot, black 8%),
+				$click: color-mix(in oklch, $col-prot, black 16%),
+			);
+		}
 
-    &.edge {
-      padding-left: 1rem;
-    }
+		&.edge {
+			padding-left: 1rem;
+		}
 
-    .section-label {
-      position: absolute;
-      top: -2em;
-      font-size: 80%;
-      color: $col-text-deut;
-      white-space: nowrap;
+		.section-label {
+			position: absolute;
+			top: -2em;
+			font-size: 80%;
+			color: $col-text-deut;
+			white-space: nowrap;
 
-      &.down { top: 2em; }
-    }
-  }
+			&.down { top: 2em; }
+		}
+	}
 
-  &.tight .edge {
-    padding-left: 0.6rem;
-  }
+	&.tight .edge {
+		padding-left: 0.6rem;
+	}
 }
 
 .layout {
-  padding-bottom: 3rem;
-  display: flex;
-  flex-flow: row nowrap;
-  gap: 2rem;
+	padding-bottom: 3rem;
+	display: flex;
+	flex-flow: row nowrap;
+	gap: 1rem;
 
-  .half {
-    height: 70vh;
+	.half {
+		height: 70vh;
 
-    &.left {
-      flex-grow: 1;
-      max-width: min(40em, 40vw);
-    }
+		&.left {
+			flex-grow: 1;
+			max-width: min(40em, 40vw);
+		}
 
-    &.right {
-      flex-grow: 3;
-    }
-  }
+		&.right {
+			flex-grow: 3;
+		}
+	}
 }
 
 article {
-  max-height: 70vh;
-  padding: 0 0.5rem 1rem;
-  overflow-y: scroll;
+	max-height: 70vh;
+	padding: 0 0.5rem 1rem;
+	overflow-y: scroll;
 
-  .padding-container {
-    // NOTE: Using `padding:` instead of `gap:` to ensure `slide` transition is smooth
-    padding-bottom: 1rem;
-  }
+	.padding-container {
+		// NOTE: Using `padding:` instead of `gap:` to ensure `slide` transition is smooth
+		padding-bottom: 1rem;
+	}
 
-  // NOTE: Overriding `article p` expanded styling
-  :global(p) {
-    margin: 0;
-  }
+	// NOTE: Overriding `article p` expanded styling
+	:global(p) {
+		margin: 0;
+	}
 }
 
 section {
-  padding: 1em 1.5em;
-  border-radius: 0.5em;
-  transition: all 0.12s ease-out;
+	padding: 1em 1.5em;
+	border-radius: 0.5em;
+	transition: all 0.12s ease-out;
 
-  &:not(.live) {
-    color: $col-text-deut !important;
-    background: $col-hover !important;
-    opacity: 50%;
-  }
-  
-  &.live {
-    opacity: 100%;
-    box-shadow: 0 1px 4px $col-line;
-  }
+	&:not(.live) {
+		color: $col-text-deut !important;
+		background: $col-hover !important;
+		opacity: 50%;
+	}
+	
+	&.live {
+		opacity: 100%;
+		box-shadow: 0 1px 4px $col-line;
+	}
 
-  :global(strong em) {
-    color: $col-deut;
-  }
+	:global(strong em) {
+		color: $col-deut;
+	}
 
-  :global(pre) {
-    margin: 1em 0;
-  }
+	:global(pre) {
+		margin: 1em 0;
+	}
 
-  :global(aside ~ br) {
-    display: none;
-  }
+	:global(aside ~ br) {
+		display: none;
+	}
 }
 
 :global(section.dyna-scripture:has(aside, aside.challenge)) {
-  color: color.change($col-manifold, $lightness: 40%);
-  background: color.change($col-manifold, $alpha: 12%);
+	color: color.change($col-manifold, $lightness: 40%);
+	background: color.change($col-manifold, $alpha: 12%);
 }
 :global(section.dyna-scripture:has(aside.solution)) {
-  color: color.change($col-yes, $lightness: 40%);
-  background: color.change($col-yes, $alpha: 12%);
+	color: color.change($col-yes, $lightness: 40%);
+	background: color.change($col-yes, $alpha: 12%);
 }
 :global(section.dyna-scripture:has(aside.note)) {
-  color: color.change($col-prot-light, $lightness: 40%);
-  background: color.change($col-prot-light, $alpha: 12%);
+	color: color.change($col-prot-light, $lightness: 40%);
+	background: color.change($col-prot-light, $alpha: 12%);
 }
 
 nav.lower {
-  padding-top: 1rem;
-  flex-flow: row nowrap;
-  justify-content: space-between;
+	padding-top: 1rem;
+	flex-flow: row nowrap;
+	justify-content: space-between;
 }
 
 
 aside.overlay {
-  padding: 3rem 5rem;
-  margin-top: 3rem;
-  height: 70vh;
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: space-between;
-  border-radius: 1em;
-  box-shadow: 0 1px 4px $col-line;
+	padding: 3rem 5rem;
+	margin-top: 3rem;
+	height: 70vh;
+	display: flex;
+	flex-flow: column nowrap;
+	justify-content: space-between;
+	border-radius: 1em;
+	box-shadow: 0 1px 4px $col-line;
 
-  h1 {
-    padding-bottom: 1em;
-    @include font-serif;
-    font-size: 400%;
-  }
+	h1 {
+		padding-bottom: 1em;
+		@include font-serif;
+		font-size: 400%;
+	}
 
-  .desc {
-    font-size: 125%;
-  }
+	.desc {
+		font-size: 125%;
+	}
 
-  nav.lower {
-    font-size: 150%;
+	nav.lower {
+		font-size: 150%;
 
-    p {
-      color: $col-text-deut;
-      font-size: 80%;
-    }
-  }
+		p {
+			color: $col-text-deut;
+			font-size: 80%;
+		}
+	}
 }
 
 </style>
